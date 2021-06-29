a0854c245bc374a5f727f9ef7d537ec9e322280c
Abel Eduardo <eduardo.9666@hotmail.com>
2021-06-29 00:41:34 -0500

    just a commit

---
+++ WebAPISample/Data/WebAPISampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPISample.Models;
+
+namespace WebAPISample.Data
+{
+    public class WebAPISampleContext : DbContext
+    {
+        public WebAPISampleContext (DbContextOptions<WebAPISampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPISample.Models.Book> Book { get; set; }
+    }
+}
